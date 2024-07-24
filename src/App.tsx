ac6fd04f90249afd8c1018c4643265a7bfd48835
alchogh <alchogh01@gmail.com>
2024-07-24 20:39:19 +0900

    🎨 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/books",
+		element: <div>도서목록</div>,
+	},
+]);
 function App() {
 	return (
 		<BookStoreThemeProvider>
 			<ThemeSwitcher />
 			<Layout>
-				<Home />
+				<RouterProvider router={router} />
 			</Layout>
 		</BookStoreThemeProvider>
 	);
